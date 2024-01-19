3c4f3c8ee29bbf0dfd0bda5142991e46ef2cc2f7
Sébastien Wagener <sebastien.wagener@gmail.com>
2024-01-19 20:28:05 +0100

    Add paths

--- src/scenes/example.tsx
+++ src/scenes/example.tsx
@@ -31,6 +31,7 @@ import {
   SimpleSignal,
   ColorSignal,
   easeInOutCubic,
+  Matrix2D,
 } from '@motion-canvas/core';
 
 
@@ -65,21 +66,21 @@ export class Glyph extends Path {
       <Path
         //ref={this.path}
         start={0.0}
-        end={0.0}
+        end={1.0}
         stroke={this.color}
         data={props.data}
         position={props.position}
-        lineWidth={1 / 20.0} // TODO why so small?
+        lineWidth={1} // TODO why so small?
         fill={() => setAlpha(this.color(), 0.0)}
       />
     );
   }
 
   public * fadeIn(duration: number) {
-    yield* tween(1 / 2 * duration, value => {
-      this.end(easeInOutCubic(value));
+    yield* tween(2 / 3 * duration, value => {
+      //this.end(easeInOutCubic(value));
     });
-    yield* tween(1 / 2 * duration, value => {
+    yield* tween(1 / 3 * duration, value => {
       this.fill(setAlpha(this.color(), easeInOutCubic(value, value)));
     });
   }
@@ -97,43 +98,43 @@ export default makeScene2D(function* (view) {
   const glyph4p2 = "M 2.453125 -4.765625 L 2.453125 -4.296875 C 2.53125 -4.296875 2.625 -4.296875 2.703125 -4.296875 C 2.875 -4.296875 3.03125 -4.265625 3.03125 -4.109375 L 3.03125 -2.859375 C 2.6875 -3.125 2.140625 -3.234375 1.75 -3.234375 C 0.734375 -3.234375 0.328125 -2.390625 0.328125 -1.609375 C 0.328125 -1.15625 0.453125 -0.6875 0.765625 -0.34375 C 1.015625 -0.078125 1.375 0.03125 1.734375 0.03125 C 2.171875 0.03125 2.671875 -0.125 3 -0.4375 L 3 0.03125 L 4.125 0.03125 L 4.125 -0.484375 L 3.8125 -0.484375 C 3.6875 -0.484375 3.546875 -0.5 3.546875 -0.625 L 3.546875 -4.84375 Z M 0.9375 -1.484375 L 0.9375 -1.5625 C 0.9375 -2.125 1.15625 -2.765625 2.03125 -2.765625 C 2.375 -2.765625 2.78125 -2.625 2.953125 -2.3125 C 2.96875 -2.28125 2.984375 -2.25 3 -2.203125 L 3 -1.25 C 3 -0.921875 2.625 -0.609375 2.359375 -0.515625 C 2.234375 -0.453125 2.09375 -0.4375 1.953125 -0.4375 C 1.65625 -0.4375 1.359375 -0.546875 1.171875 -0.765625 C 1 -0.96875 0.96875 -1.21875 0.9375 -1.484375 Z M 0.9375 -1.484375";
   const glyph5p1 = "M 3.90625 -3.4375 L 3.875 -3.46875 C 3.34375 -3.453125 2.75 -3.421875 2.3125 -3.421875 C 1.25 -3.421875 1.25 -3.453125 0.6875 -3.46875 L 0.640625 -3.4375 L 0.640625 -3.203125 L 0.6875 -3.171875 C 1.34375 -3.1875 1.828125 -3.203125 1.96875 -3.203125 C 2 -3.203125 2.015625 -3.1875 2.015625 -3.171875 C 2.03125 -3.015625 2.03125 -2.296875 2.03125 -1.890625 C 2.03125 -1.265625 2 -0.625 1.953125 0 L 2.015625 0.046875 L 2.609375 -0.078125 C 2.5625 -0.6875 2.53125 -1.578125 2.53125 -2.1875 C 2.53125 -2.578125 2.546875 -3.078125 2.546875 -3.15625 C 2.546875 -3.1875 2.5625 -3.203125 2.609375 -3.203125 C 2.734375 -3.203125 2.859375 -3.203125 3.859375 -3.15625 L 3.90625 -3.1875 Z M 3.90625 -3.4375";
 
-
   const terminal1 = createRef<Glyph>();
   const terminal2 = createRef<Glyph>();
   const terminal3 = createRef<Glyph>();
+  const path4 = createRef<Path>();
+  const path5 = createRef<Path>();
   const terminal6 = createRef<Glyph>();
+  const path7 = createRef<Path>();
+  const path8 = createRef<Path>();
   const terminal9 = createRef<Glyph>();
   const terminal10 = createRef<Glyph>();
+  const path11 = createRef<Path>();
+  const path12 = createRef<Path>();
   const terminal13a = createRef<Glyph>();
   const terminal13b = createRef<Glyph>();
   const terminal14 = createRef<Glyph>();
-  // view.add(
-  //   <Rect
-  //     position={[+10, -10]}
-  //   >
-  //     <Glyph
-  //       ref={terminal2}
-  //       color={"rgb(56.001282%,82.843018%,35.496521%)"}
-  //       //fill={"rgb(56.001282%,82.843018%,35.496521%)"}
-  //       data={glyphT}
-  //       position={[10, 10]}
-  //     />
-  //     <Glyph
-  //       ref={terminal3}
-  //       color={"rgb(56.001282%,82.843018%,35.496521%)"}
-  //       //fill={"rgb(56.001282%,82.843018%,35.496521%)"}
-  //       data={glyphPrime}
-  //       position={[138.578 - 131.786 + 10, 78.042 - 81.658 + 10]}
-  //     />
+  const path15 = createRef<Path>();
+  const path16 = createRef<Path>();
+  const path17 = createRef<Path>();
+  const path18 = createRef<Path>();
 
+  const matrix4 = new Matrix2D(1, 0, 0, - 1, 105.093, 95.844);
+  const matrix5 = new Matrix2D(0.87663, 0.48108, 0.48108, -0.87663, 126.84604, 73.48473);
+  const matrix7 = new Matrix2D(1, 0, 0, -1, 105.093, 95.844);
+  const matrix8 = new Matrix2D(0.0094, 0.99991, 0.99991, -0.0094, 136.57683, 69.88415);
+  const matrix11 = new Matrix2D(1, 0, 0, -1, 105.093, 95.844);
+  const matrix12 = new Matrix2D(0.70706, 0.70712, 0.70712, -0.70706, 63.90844, 71.18723);
+  const matrix15 = new Matrix2D(1, 0, 0, -1, 105.093, 95.844);
+  const matrix16 = new Matrix2D(-0.50002, 0.866, 0.866, 0.50002, 76.77458, 70.52056);
+  const matrix17 = new Matrix2D(1, 0, 0, -1, 105.093, 95.844);
+  const matrix18 = new Matrix2D(-0.85493, -0.51865, -0.51865, 0.85493, 79.7682, 83.68824);
 
-  //   </Rect>
+  // TODO create component for arrow
 
-  // );
   view.add(
     <Rect
-      scale={1}
-      position={[-1000 / 6, -800 / 6]}
+      scale={20}
+      position={[-900, -1000]} // TODO automatically compute to fill screen
     >
       <Glyph
         ref={terminal1}
@@ -153,12 +154,70 @@ export default makeScene2D(function* (view) {
         data={glyphPrime}
         position={[138.578, 78.042]}
       />
+      <Path
+        ref={path4}
+        stroke="rgb(56.001282%,82.843018%,35.496521%)" //
+        data="M -25.495344 21.234625 C -9.62425 30.863531 5.653094 31.195563 21.578875 22.453375"
+        // https://developer.mozilla.org/en-US/docs/Web/CSS/transform-function/matrix
+        // The values represent the following functions: matrix(scaleX(), skewY(), skewX(), scaleY(), translateX(), translateY())
+        scale={matrix4.scaling}
+        position={matrix4.translation}
+        rotation={matrix4.rotation}
+        skew={[matrix4.skewX, matrix4.skewY]}
+        lineWidth={0.3985}
+        lineCap={"butt"}
+        lineJoin={"miter"}
+        lineDash={[2.78952, 1.59401]}
+      // TODO miterlimit:10
+      />
+      <Path
+        ref={path5}
+        data={"M -2.072826 2.392012 C -1.694403 0.955427 -0.850274 0.277938 0.00124617 0.00108884 C -0.851651 -0.279815 -1.692937 -0.955386 -2.073359 -2.389552 "}
+        stroke={"rgb(56.001282%,82.843018%,35.496521%)"}
+        lineCap={"round"}
+        lineJoin={"round"}
+        lineWidth={0.3985}
+        scale={matrix5.scaling}
+        position={matrix5.translation}
+        rotation={matrix5.rotation}
+        skew={[matrix5.skewX, matrix5.skewY]}
+      // TODO miterlimit:10
+      />
       <Glyph
         ref={terminal6}
         color={"rgb(56.001282%,82.843018%,35.496521%)"}
         data={glyphF}
         position={[101.289, 64.653]}
       />
+      <Path
+        ref={path7}
+        lineWidth={0.3985}
+        scale={matrix7.scaling}
+        position={matrix7.translation}
+        rotation={matrix7.rotation}
+        skew={[matrix7.skewX, matrix7.skewY]}
+        lineCap={"butt"}
+        lineJoin={"miter"}
+        stroke={"rgb(100%,100%,100%)"}
+        data={"M -33.268781 25.152594 C -33.628156 63.176031 31.129656 63.785406 31.481219 26.160406"}
+      // TODO miterlimit:10
+      />
+      <Path
+        ref={path8}
+        //" d=" " transform="matrix(0.0094,0.99991,0.99991,-0.0094,136.57683,69.88415)" />
+        lineWidth={0.3985}
+        // TODO re-enable scaling (-0.01, -0.01)
+        //scale={matrix8.scaling}
+        position={matrix8.translation}
+        rotation={matrix8.rotation}
+        skew={[matrix8.skewX, matrix8.skewY]}
+        lineCap={"round"}
+        lineJoin={"round"}
+        stroke={"rgb(100%,100%,100%)"}
+        data={"M -2.072788 2.392088 C -1.691734 0.954783 -0.850355 0.278845 -0.00132533 0.00130758 C -0.8517 -0.279787 -1.694066 -0.955523 -2.070861 -2.38961 "}
+      // TODO miterlimit:10
+      />
+
       <Glyph
         ref={terminal9}
         color={"rgb(100%,100%,100%)"}
@@ -170,6 +229,35 @@ export default makeScene2D(function* (view) {
         color={"rgb(100%,100%,100%)"}
         data={glyphPrime2}
         position={[104.731, 36.858]}
+      />
+      <Path
+        ref={path11}
+        //     stroke-linecap:butt;stroke-linejoin:miter;stroke:;stroke-opacity:1;stroke-dasharray:;stroke-miterlimit:10;" d= />
+        //" d=" " transform="matrix(0.0094,0.99991,0.99991,-0.0094,136.57683,69.88415)" />
+        lineWidth={0.3985}
+        scale={matrix11.scaling}
+        position={matrix11.translation}
+        rotation={matrix11.rotation}
+        skew={[matrix11.skewX, matrix11.skewY]}
+        lineCap={"butt"}
+        lineJoin={"miter"}
+        stroke={"rgb(96.42334%,52.000427%,78.35083%)"}
+        lineDash={[2.78952, 1.59401]}
+        data={"M -39.909406 10.105719 C -103.760969 -53.745844 -104.893781 88.371344 -41.323469 24.797125 "}
+      // TODO miterlimit:10
+      />
+      <Path
+        ref={path12}
+        lineWidth={0.3985}
+        scale={matrix12.scaling}
+        position={matrix12.translation}
+        rotation={matrix12.rotation}
+        skew={[matrix12.skewX, matrix12.skewY]}
+        lineCap={"round"}
+        lineJoin={"round"}
+        stroke={"rgb(96.42334%,52.000427%,78.35083%)"}
+        data={"M -2.07311 2.390239 C -1.694628 0.956672 -0.849365 0.277207 0.00140448 0.00102274 C -0.849342 -0.280758 -1.694547 -0.954771 -2.070145 -2.391132 "}
+      // TODO miterlimit:10
       />
       <Glyph
         ref={terminal13a}
@@ -188,6 +276,64 @@ export default makeScene2D(function* (view) {
         color={"rgb(96.42334%,52.000427%,78.35083%)"}
         data={glyph5p1}
         position={[9.18, 81.482]}
+      />
+      <Path
+        ref={path15}
+        // rgb(100%,100%,100%)" d="M -35.518781 25.152594 C -44.128156 57.265875 -11.792219 53.941656 -28.218 25.496344 " />
+
+        lineWidth={0.3985}
+        scale={matrix15.scaling}
+        position={matrix15.translation}
+        rotation={matrix15.rotation}
+        skew={[matrix15.skewX, matrix15.skewY]}
+        lineCap={"butt"}
+        lineJoin={"miter"}
+        stroke={"rgb(96.42334%,52.000427%,78.35083%)"}
+        lineDash={[2.78952, 1.59401]}
+        data={"M -39.909406 10.105719 C -103.760969 -53.745844 -104.893781 88.371344 -41.323469 24.797125 "}
+      // TODO miterlimit:10
+      />
+      <Path
+        ref={path16}
+        // rgb(100%,100%,100%)" d="M -2.072689 2.392432 C -1.694136 0.955585 -0.848525 0.280077 -0.000319632 -0.00150384 C -0.849891 -0.280036 -1.694767 -0.957308 -2.070622 -2.392207 " />
+
+        lineWidth={0.3985}
+        scale={matrix16.scaling}
+        position={matrix16.translation}
+        rotation={matrix16.rotation}
+        skew={[matrix16.skewX, matrix16.skewY]}
+        lineCap={"round"}
+        lineJoin={"round"}
+        stroke={"rgb(96.42334%,52.000427%,78.35083%)"}
+        data={"M -2.07311 2.390239 C -1.694628 0.956672 -0.849365 0.277207 0.00140448 0.00102274 C -0.849342 -0.280758 -1.694547 -0.954771 -2.070145 -2.391132 "}
+      // TODO miterlimit:10
+      />
+      <Path
+        ref={path17}
+        lineWidth={0.3985}
+        scale={matrix17.scaling}
+        position={matrix17.translation}
+        rotation={matrix17.rotation}
+        skew={[matrix17.skewX, matrix17.skewY]}
+        lineCap={"butt"}
+        lineJoin={"miter"}
+        stroke={"rgb(96.42334%,52.000427%,78.35083%)"}
+        lineDash={[2.78952, 1.59401]}
+        data={"M 21.926531 11.230719 C 5.653094 2.297125 -9.62425 2.629156 -25.1555 12.051031 "}
+      // TODO miterlimit:10
+      />
+      <Path
+        ref={path18}
+        lineWidth={0.3985}
+        scale={matrix18.scaling}
+        position={matrix18.translation}
+        rotation={matrix18.rotation}
+        skew={[matrix18.skewX, matrix18.skewY]}
+        lineCap={"round"}
+        lineJoin={"round"}
+        stroke={"rgb(96.42334%,52.000427%,78.35083%)"}
+        data={"M -2.073892 2.391692 C -1.691708 0.955831 -0.850953 0.277919 -0.000754398 -0.00132323 C -0.850912 -0.279485 -1.6931 -0.954893 -2.073322 -2.391796 "}
+      // TODO miterlimit:10
       />
 
     </Rect>,
